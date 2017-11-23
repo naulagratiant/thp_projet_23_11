8af2fd1f89467190a241a5348e8dd7e929828c84
Naula <naulagratiant@gmail.com>
2017-11-23 22:31:25 +0100

    modif

--- tabs.js
+++ tabs.js
@@ -1,16 +1,6 @@
-console.log Naula
-
-
-$(document).ready(function(){
-	
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
-
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
-
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	})
-
-})
+alert ("naula")
+$( ".tab-link" ).click(function() {
+  $( ".tab-content" ).slideDown( "slow", function() {
+    // Animation complete.
+  });
+});
